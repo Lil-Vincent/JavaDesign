f4d8d8f200fda76f01a438814dfadbf02092c4dc
Lil <1378951829@qq.com>
2021-11-18 09:52:29 +0800

    初始化
    初始化

--- src/main/java/策略模式/test.java
+++ src/main/java/策略模式/test.java
@@ -5,6 +5,7 @@ package 策略模式;
  * @date 2021/11/17 23:46
  */
 public class test {
+    //撒旦打撒
     public static void main(String[] args) {
         IPayStrategy iPayStrategy = PayEnum.getValue("aliPay");
         iPayStrategy.queryBlance();
